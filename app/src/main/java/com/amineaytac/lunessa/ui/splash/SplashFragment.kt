ff9ff3d3adbf231e0d2f22712a69f3ba3b966f9d
amine <aytacamine633@gmail.com>
2025-03-03 09:18:43 +0300

    add nav_graph.xml

--- app/src/main/java/com/amineaytac/lunessa/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/amineaytac/lunessa/ui/splash/SplashFragment.kt
@@ -5,15 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.amineaytac.lunessa.R
+import com.amineaytac.lunessa.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
+
+    private lateinit var binding: FragmentSplashBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+    ): View {
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
 }
