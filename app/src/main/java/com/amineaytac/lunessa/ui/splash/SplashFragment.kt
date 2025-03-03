8c0bc3761ffd813870cc9914e38b29db27caf9ca
amine <aytacamine633@gmail.com>
2025-03-03 10:25:19 +0300

    edit SplashFragment and nav_graph.xml

--- app/src/main/java/com/amineaytac/lunessa/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/amineaytac/lunessa/ui/splash/SplashFragment.kt
@@ -1,22 +1,45 @@
 package com.amineaytac.lunessa.ui.splash
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.amineaytac.lunessa.R
 import com.amineaytac.lunessa.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
 
     private lateinit var binding: FragmentSplashBinding
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentSplashBinding.inflate(inflater, container, false)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            if (onBoardingIsFinished()) {
+                //findNavController().navigate()
+            } else {
+                findNavController().navigate(R.id.action_splashFragment_to_onBoardingFragment)
+            }
+        }, 3000)
+
         return binding.root
     }
 
+    private fun onBoardingIsFinished(): Boolean {
+        val sharedPreferences =
+            requireActivity().getSharedPreferences(
+                getString(R.string.on_boarding),
+                Context.MODE_PRIVATE
+            )
+        return sharedPreferences.getBoolean(getString(R.string.finished), false)
+    }
 }
