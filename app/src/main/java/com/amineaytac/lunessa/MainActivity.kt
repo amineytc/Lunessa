b77d5428feec1342f637df0a1d5e1cc2477cabdf
amine <aytacamine633@gmail.com>
2025-03-14 18:13:40 +0300

    edit Extensions.kt, activity_main.xml, nav_graph.xml and MainActivity.kt

--- app/src/main/java/com/amineaytac/lunessa/MainActivity.kt
+++ app/src/main/java/com/amineaytac/lunessa/MainActivity.kt
@@ -2,12 +2,45 @@ package com.amineaytac.lunessa
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.amineaytac.lunessa.databinding.ActivityMainBinding
+import com.amineaytac.lunessa.util.gone
+import com.amineaytac.lunessa.util.visible
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavView.itemActiveIndicatorColor = null
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        NavigationUI.setupWithNavController(binding.bottomNavView, navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.splashFragment,
+                R.id.loginFragment2,
+                R.id.signupFragment,
+                R.id.basketFragment2,
+                R.id.onBoardingFragment -> {
+                    binding.bottomNavView.gone()
+                }
+
+                else -> {
+                    binding.bottomNavView.visible()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/amineaytac/lunessa/util/Extensions.kt
+++ app/src/main/java/com/amineaytac/lunessa/util/Extensions.kt
@@ -1,6 +1,7 @@
 package com.amineaytac.lunessa.util
 
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
 
@@ -8,4 +9,12 @@ fun NavController.safeNavigateWithArgs(direction: NavDirections, bundle: Bundle?
     currentDestination?.getAction(direction.actionId)?.run {
         navigate(direction.actionId, bundle)
     }
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
+
+fun View.visible() {
+    visibility = View.VISIBLE
 }
