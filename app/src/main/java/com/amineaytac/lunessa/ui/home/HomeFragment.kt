122ca1425938b43ff6147df36c9f96ca2554d2aa
amine <aytacamine633@gmail.com>
2025-03-30 00:33:27 +0300

    edit NetworkModule and HomeFragment

--- app/src/main/java/com/amineaytac/lunessa/ui/home/HomeFragment.kt
+++ app/src/main/java/com/amineaytac/lunessa/ui/home/HomeFragment.kt
@@ -1,21 +1,55 @@
 package com.amineaytac.lunessa.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.amineaytac.lunessa.R
+import androidx.fragment.app.viewModels
+import com.amineaytac.lunessa.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        callInitialViewModelFunctions()
+        observeUi()
+    }
+
+    private fun callInitialViewModelFunctions() {
+        viewModel.getAllMakeupProducts()
+    }
+
+    private fun observeUi() {
+        viewModel.makeupScreenUiState.observe(viewLifecycleOwner) {
+            when {
+                it.isError -> {
+                    Log.d("home_data3", it.errorMessage.toString())
+                }
+
+                it.isLoading -> {
+                    Log.d("home_data2", "loading")
+                }
+
+                else -> {
+                    Log.d("home_data1", it.products.toString())
+                }
+            }
+        }
     }
 }
