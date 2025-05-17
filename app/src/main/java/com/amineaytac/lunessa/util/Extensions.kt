a63f18487733ce6519fedc1638ed01ae780ce3af
amine <aytacamine633@gmail.com>
2025-05-17 16:12:16 +0300

    edit Extensions.kt, strings.xml and themes.xml

--- app/src/main/java/com/amineaytac/lunessa/util/Extensions.kt
+++ app/src/main/java/com/amineaytac/lunessa/util/Extensions.kt
@@ -1,7 +1,9 @@
 package com.amineaytac.lunessa.util
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
 
@@ -17,4 +19,13 @@ fun View.gone() {
 
 fun View.visible() {
     visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
+
+fun View.hideKeyboard() {
+    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+    imm?.hideSoftInputFromWindow(windowToken, 0)
 }
